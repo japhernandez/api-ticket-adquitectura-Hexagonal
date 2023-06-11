623b792bf7003d76edf585dea51207484694f961
Darlin Ruben <chinausto@gmail.com>
2023-06-11 16:41:07 -0400

    verifyToken

--- src/application/auth/login.services.ts
+++ src/application/auth/login.services.ts
@@ -1,72 +1,32 @@
-import { autoInjectable } from 'tsyringe';
+import { autoInjectable } from "tsyringe";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import { User } from '@/domain/User/User';
-import { UserRepository } from '@/domain/User/UserRespository';
-import { CustomError } from '@/domain/errors/CustomError';
+import { User } from "@/domain/User/User";
+import { UserRepository } from "@/domain/User/UserRespository";
+import { CustomError } from "@/domain/errors/CustomError";
 
-/**
- * Servicio de autenticación y inicio de sesión.
- */
 @autoInjectable()
 export class LoginService {
-  /**
-   * Crea una instancia del servicio LoginService.
-   * @param userRepository - Repositorio de usuarios.
-   */
   constructor(private userRepository: UserRepository) {}
 
-  /**
-   * Realiza el inicio de sesión de un usuario y genera tokens de acceso y refresco.
-   * @param email - Correo electrónico del usuario.
-   * @param password - Contraseña del usuario.
-   * @returns Un objeto que contiene el usuario, el token de acceso y el token de refresco.
-   * @throws {CustomError} Si el usuario no existe o la contraseña es inválida.
-   */
-  async login(email: string, password: string): Promise<{ user: User; accessToken: string; refreshToken: string }> {
+  async login(email: string, password: string): Promise<{ user: User, token: string }> {
     const user = await this.userRepository.findByEmail(email);
     if (!user) {
-      throw new CustomError(404, 'User not found');
+      throw new CustomError(404, "User not found");
     }
 
     const passwordMatch = await bcrypt.compare(password, user.password);
     if (!passwordMatch) {
-      throw new CustomError(401, 'Invalid password');
+      throw new CustomError(401, "Invalid password");
     }
 
-    const accessToken = this.generateAccessToken(user);
-    const refreshToken = this.generateRefreshToken(user);
-
-    return { user, accessToken, refreshToken };
-  }
-
-  /**
-   * Genera un token de acceso para el usuario.
-   * @param user - Objeto de usuario.
-   * @returns El token de acceso generado.
-   * @throws {CustomError} Si no se puede generar el token debido a una configuración incorrecta.
-   */
-  private generateAccessToken(user: User): string {
-    if (!process.env.JWT_SECRET) {
-      throw new CustomError(500, 'Failed to generate token. JWT_SECRET is missing.');
+    const jwtSecret = process.env.JWT_SECRET;
+    if (!jwtSecret) {
+      throw new CustomError(500, "Failed to generate token. JWT_SECRET is missing.");
     }
 
-    const accessToken = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    return accessToken;
-  }
+    const token = jwt.sign({ userId: user.id }, jwtSecret, { expiresIn: '1h' });
 
-  /**
-   * Genera un token de refresco para el usuario.
-   * @param user - Objeto de usuario.
-   * @returns El token de refresco generado.
-   * @throws {CustomError} Si no se puede generar el token debido a una configuración incorrecta.
-   */
-  private generateRefreshToken(user: User): string {
-    if (!process.env.JWT_SECRET) {
-      throw new CustomError(500, 'Failed to generate token. JWT_SECRET is missing.');
-    }
-
-    const refreshToken = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '7d' });
-    return refreshToken;
+    return { user, token };
   }
 }
--- src/infrastructure/presentation/controllers/auth/login.controller.ts
+++ src/infrastructure/presentation/controllers/auth/login.controller.ts
@@ -1,16 +1,10 @@
 import { Request, Response } from 'express';
 import { LoginService } from '@/application/auth/login.services';
 import { CustomError } from '@/domain/errors/CustomError';
-import { User } from '@/domain/User/User';
-
 /**
  * Controlador para manejar la solicitud de inicio de sesión.
  */
 export class LoginController {
-    /**
-     * Crea una instancia del controlador LoginController.
-     * @param loginService - Servicio de inicio de sesión.
-     */
     constructor(private loginService: LoginService) { }
 
     /**
@@ -24,37 +18,37 @@ export class LoginController {
             const { email, password } = req.body;
 
             // Realizar el inicio de sesión utilizando el servicio de autenticación
-            const { user, accessToken, refreshToken } = await this.loginService.login(email, password);
+            const { user, token } = await this.loginService.login(email, password);
 
-            // Enviar una respuesta exitosa con el usuario y los tokens
-            this.sendSuccessResponse(res, 'Login successful', { user, accessToken, refreshToken });
+            // Enviar una respuesta exitosa con el usuario y el token de JWT
+            this.sendSuccessResponse(res, 'Login successful', { user, token });
         } catch (err) {
             console.error(err);
             if (err instanceof CustomError) {
                 this.sendErrorResponse(res, err.statusCode, err.message);
             } else {
-                this.sendErrorResponse(res, 500, 'Failed to login');
+                this.sendErrorResponse(res, 500, "failed to login");
             }
         }
     }
 
     /**
-     * Envía una respuesta exitosa al cliente.
+     * Enviar una respuesta exitosa.
      * @param res - Objeto de respuesta de Express.
      * @param message - Mensaje de éxito.
-     * @param data - Datos adicionales a enviar en la respuesta.
+     * @param data - Datos opcionales para incluir en la respuesta.
      */
     private sendSuccessResponse(res: Response, message: string, data?: any): void {
         res.status(200).json({ message, data });
     }
 
     /**
-     * Envía una respuesta de error al cliente.
+     * Enviar una respuesta de error.
      * @param res - Objeto de respuesta de Express.
-     * @param statusCode - Código de estado HTTP del error.
+     * @param statusCode - Código de estado de HTTP para la respuesta.
      * @param message - Mensaje de error.
      */
     private sendErrorResponse(res: Response, statusCode: number, message: string): void {
-        res.status(statusCode).json({ error: message });
+        res.status(statusCode).json({ message });
     }
 }
