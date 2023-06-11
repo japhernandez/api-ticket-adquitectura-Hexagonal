68be64601db8121fcc1ca0cf407d2c45b9d00156
Darlin Ruben <chinausto@gmail.com>
2023-06-11 17:14:54 -0400

    refrestToket

--- src/application/auth/login.services.ts
+++ src/application/auth/login.services.ts
@@ -1,31 +1,56 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import { User } from "@/domain/User/User";
-import { UserRepository } from "@/domain/User/UserRespository";
-import { CustomError } from "@/domain/errors/CustomError";
+import { User } from '@/domain/User/User';
+import { UserRepository } from '@/domain/User/UserRespository';
+import { CustomError } from '@/domain/errors/CustomError';
 
-
+/**
+ * Clase que representa el servicio de inicio de sesión.
+ */
 export class LoginService {
-  constructor(private userRepository: UserRepository) {}
+  /**
+   * Constructor de la clase LoginService.
+   * @param userRepository Repositorio de usuarios.
+   */
+  constructor(private userRepository: UserRepository) { }
 
-  async login(email: string, password: string): Promise<{ user: User, token: string }> {
+  /**
+   * Método asincrónico para realizar el inicio de sesión.
+   * @param email Dirección de correo electrónico del usuario.
+   * @param password Contraseña del usuario.
+   * @returns Una promesa que se resuelve en un objeto con las propiedades 'user' y 'token'.
+   * @throws CustomError si se produce algún error durante el proceso de inicio de sesión.
+   */
+  async login(email: string, password: string): Promise<{ user: User; token: string }> {
+    // Buscar el usuario por su dirección de correo electrónico
     const user = await this.userRepository.findByEmail(email);
+
+    // Si no se encuentra ningún usuario, lanzar un error personalizado
     if (!user) {
-      throw new CustomError(401, "invalid email ");
+      throw new CustomError(404, 'Invalid email');
     }
 
+    // Comparar la contraseña proporcionada con la contraseña almacenada del usuario
     const passwordMatch = await bcrypt.compare(password, user.password);
+
+    // Si las contraseñas no coinciden, lanzar un error personalizado
     if (!passwordMatch) {
-      throw new CustomError(401, "Invalid password");
+      throw new CustomError(401, 'Invalid password');
     }
 
+    // Verificar la existencia de la variable de entorno JWT_SECRET
     const jwtSecret = process.env.JWT_SECRET;
+
+    // Si JWT_SECRET no está definido, lanzar un error personalizado
     if (!jwtSecret) {
-      throw new CustomError(500, "Failed to generate token. JWT_SECRET is missing.");
+      throw new CustomError(500, 'Failed to generate token. JWT_SECRET is missing.');
     }
 
+    // Generar un token JWT firmado que contiene el ID de usuario
     const token = jwt.sign({ userId: user.id }, jwtSecret, { expiresIn: '1h' });
 
+    // Devolver el usuario y el token como resultado del inicio de sesión
     return { user, token };
   }
 }
+
