8b23173a1fe97bfa51be6ebd3345def774cd23cb
Darlin Ruben <chinausto@gmail.com>
2023-06-11 15:46:12 -0400

    refrestToket

--- src/infrastructure/presentation/controllers/auth/login.controller.ts
+++ src/infrastructure/presentation/controllers/auth/login.controller.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from 'express';
 import { LoginService } from '@/application/auth/login.services';
 import { CustomError } from '@/domain/errors/CustomError';
+import { User } from '@/domain/User/User';
+
 /**
  * Controlador para manejar la solicitud de inicio de sesión.
  */
 export class LoginController {
+    /**
+     * Crea una instancia del controlador LoginController.
+     * @param loginService - Servicio de inicio de sesión.
+     */
     constructor(private loginService: LoginService) { }
 
     /**
@@ -18,37 +24,37 @@ export class LoginController {
             const { email, password } = req.body;
 
             // Realizar el inicio de sesión utilizando el servicio de autenticación
-            const { user, token } = await this.loginService.login(email, password);
+            const { user, accessToken, refreshToken } = await this.loginService.login(email, password);
 
-            // Enviar una respuesta exitosa con el usuario y el token de JWT
-            this.sendSuccessResponse(res, 'Login successful', { user, token });
+            // Enviar una respuesta exitosa con el usuario y los tokens
+            this.sendSuccessResponse(res, 'Login successful', { user, accessToken, refreshToken });
         } catch (err) {
             console.error(err);
             if (err instanceof CustomError) {
                 this.sendErrorResponse(res, err.statusCode, err.message);
             } else {
-                this.sendErrorResponse(res, 500, "failed to login");
+                this.sendErrorResponse(res, 500, 'Failed to login');
             }
         }
     }
 
     /**
-     * Enviar una respuesta exitosa.
+     * Envía una respuesta exitosa al cliente.
      * @param res - Objeto de respuesta de Express.
      * @param message - Mensaje de éxito.
-     * @param data - Datos opcionales para incluir en la respuesta.
+     * @param data - Datos adicionales a enviar en la respuesta.
      */
     private sendSuccessResponse(res: Response, message: string, data?: any): void {
         res.status(200).json({ message, data });
     }
 
     /**
-     * Enviar una respuesta de error.
+     * Envía una respuesta de error al cliente.
      * @param res - Objeto de respuesta de Express.
-     * @param statusCode - Código de estado de HTTP para la respuesta.
+     * @param statusCode - Código de estado HTTP del error.
      * @param message - Mensaje de error.
      */
     private sendErrorResponse(res: Response, statusCode: number, message: string): void {
-        res.status(statusCode).json({ message });
+        res.status(statusCode).json({ error: message });
     }
 }
