58c09dc7cdf288df25a38fa99f650eb56df670e8
Darlin Ruben <chinausto@gmail.com>
2023-06-11 17:10:57 -0400

    refrestToket

--- src/application/auth/login.services.ts
+++ src/application/auth/login.services.ts
@@ -1,18 +1,17 @@
-import { autoInjectable } from "tsyringe";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { User } from "@/domain/User/User";
 import { UserRepository } from "@/domain/User/UserRespository";
 import { CustomError } from "@/domain/errors/CustomError";
 
-@autoInjectable()
+
 export class LoginService {
   constructor(private userRepository: UserRepository) {}
 
   async login(email: string, password: string): Promise<{ user: User, token: string }> {
     const user = await this.userRepository.findByEmail(email);
     if (!user) {
-      throw new CustomError(404, "User not found");
+      throw new CustomError(401, "invalid email ");
     }
 
     const passwordMatch = await bcrypt.compare(password, user.password);
--- src/application/auth/register.services.ts
+++ src/application/auth/register.services.ts
@@ -1,4 +1,3 @@
-import { autoInjectable } from "tsyringe";
 import { User } from "@/domain/User/User";
 import { UserRepository } from "@/domain/User/UserRespository";
 import { CustomError } from "@/domain/errors/CustomError";
@@ -6,7 +5,7 @@ import { CustomError } from "@/domain/errors/CustomError";
 /**
  * Servicio de registro de usuarios.
  */
-@autoInjectable()
+
 export class RegisterService {
     constructor(private userRepository: UserRepository) { }
 
