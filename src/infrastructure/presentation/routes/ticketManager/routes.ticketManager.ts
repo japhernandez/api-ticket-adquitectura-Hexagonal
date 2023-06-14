4f1886fc030dc14e4b99c05b66908ee6fe279429
Darlin Ruben <chinausto@gmail.com>
2023-06-14 16:25:25 -0400

    ticketManager003

--- src/infrastructure/presentation/routes/ticketManager/routes.ticketManager.ts
+++ src/infrastructure/presentation/routes/ticketManager/routes.ticketManager.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { createTicket } from "../../../dependencies/tockeManager/ticketManager.dependencies";
-
+import VerifyToken from "../../../middlewares/verifyToken";
 const TicketManagetRoutes = Router();
 
 TicketManagetRoutes
